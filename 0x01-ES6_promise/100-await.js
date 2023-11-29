8a4c12ccbeb7ab3dff305ca3ffa0dfe7927d7cd7
0marSalah <omar.salah1597@gmail.com>
2023-11-29 17:40:42 +0200

    10. Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,11 +4,19 @@ import { uploadPhoto, createUser } from './utils';
  * @return {Object} Promise
 */
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  const response = {
-    photo,
-    user,
-  };
+  let response;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    response = {
+      photo,
+      user,
+    };
+  } catch (error) {
+    response = {
+      photo: null,
+      user: null,
+    };
+  }
   return response;
 }
