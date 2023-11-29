6be1f70cf0191545b8ab83737973fc360af4b734
0marSalah <omar.salah1597@gmail.com>
2023-11-29 16:33:16 +0200

    8. Throw error/try catch

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,9 +3,15 @@
  * @param {Number} denominator
  */
 export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
   try {
     return numerator / denominator;
   } catch (error) {
     return error;
   }
 }
+
+console.log(divideFunction(10, 2));
+console.log(divideFunction(10, 0));
