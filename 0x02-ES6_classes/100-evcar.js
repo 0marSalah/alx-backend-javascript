cad23fdcfd301c272d66a548eecd7d6fa3d2423e
0marSalah <omar.salah1597@gmail.com>
2023-12-01 18:08:56 +0200

    fix EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,25 +1,16 @@
+/* eslint-disable */
 import Car from './10-car';
 
-export class EVCar extends Car {
-  /**
-   * @param {string} brand
-   * @param {string} motor
-   * @param {string} color
-   * @param {number} range
-   */
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  /**
-   * @returns {string} - new Car instance
-   */
   cloneCar() {
     return new Car();
   }
 }
-
 
 const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
 console.log(ec1);
