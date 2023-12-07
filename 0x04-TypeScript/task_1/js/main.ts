a708cfdefff1045baffa55634297c86afa64e9c5
0marSalah <omar.salah1597@gmail.com>
2023-12-07 21:38:15 +0200

    Add interfaces, classes, and functions for Teacher and StudentClass

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,9 +8,49 @@ interface Teacher {
   [key: string]: any;
 }
 
-const teacher1: Teacher = {
-  firstName: "Abo",
-  lastName: "Aubaida",
-  fullTimeEmployee: true,
-  location: "Gaza"
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: PrintTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentClassI {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+interface StudentClassConstructorI {
+  new(firstName: string, lastName: string): StudentClassI;
+}
+
+class StudentClass implements StudentClassI {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 };
+
+const StudentClassConstructor: StudentClassConstructorI = StudentClass;
+
+const student = new StudentClassConstructor("Omar", "Salah");
+console.log(student.workOnHomework());
+console.log(student.displayName());
